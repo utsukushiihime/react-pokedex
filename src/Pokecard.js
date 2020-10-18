eec33bf9e1e00fff55b4dda1962f6d90a2380ecc
creattech <crystalsmcneil@gmail.com>
2020-10-18 00:30:53 -0500

    adds pokemon and minor card styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
-import './Pokecard.css';
-const PokeAPI = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+import React, { Component } from "react";
+import "./Pokecard.css";
+const PokeAPI =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class Pokecard extends Component {
-    render () {
-        let imgSrc = `${PokeAPI}${this.props.id}.png`;
-        return (
-            
-            <div className="Pokecard">
-                <h1>{this.props.name}</h1>
-                <img src={imgSrc} alg={this.props.name} />
-                <div>Type: {this.props.type}</div>
-                <div>EXP: {this.props.exp}</div>
-            </div>
-        )
-    }
+  render() {
+    let imgSrc = `${PokeAPI}${this.props.id}.png`;
+    return (
+      <div className="Pokecard">
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <img src={imgSrc} alg={this.props.name} />
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP: {this.props.exp}</div>
+      </div>
+    );
+  }
 }
 
-
 export default Pokecard;
-
-
